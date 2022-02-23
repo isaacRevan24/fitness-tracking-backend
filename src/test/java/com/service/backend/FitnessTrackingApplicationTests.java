82cb8740056e033aef6a1fd3cac8eeaa4959808b
Severiano Atencio <satencio@consultec-ti.com>
2022-02-22 22:25:26 -0500

    proyecto spring base

---
+++ src/test/java/com/service/backend/FitnessTrackingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.service.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitnessTrackingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
