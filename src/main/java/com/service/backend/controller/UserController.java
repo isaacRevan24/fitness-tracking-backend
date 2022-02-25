973e6fc1362789f7853d6315de06de1a46b3f679
Severiano Atencio <satencio@consultec-ti.com>
2022-02-24 22:48:23 -0500

    add: base de controller

--- src/main/java/com/service/backend/controller/UserController.java
+++ src/main/java/com/service/backend/controller/UserController.java
@@ -1,4 +1,28 @@
 package com.service.backend.controller;
 
+import com.service.backend.controller.dto.StatusDTO;
+import com.service.backend.controller.entity.BaseResponseEntity;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Severiano Atencio
+ */
+@RestController
+@RequestMapping("v1/fitness")
 public class UserController {
+
+    @GetMapping(path = "/health-check")
+    public ResponseEntity<BaseResponseEntity> healthCheck() {
+
+        var status = new StatusDTO("fit-00", "success", HttpStatus.OK);
+        var response = new BaseResponseEntity();
+        response.setStatus(status);
+
+        return ResponseEntity.status(status.getHttpStatus()).body(response);
+
+    }
 }
