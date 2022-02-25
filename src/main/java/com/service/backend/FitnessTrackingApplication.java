aa310e339be96c99605fd2432096ca5d410f71c4
Severiano Atencio <satencio@consultec-ti.com>
2022-02-24 23:05:43 -0500

    ad: author in main class

--- src/main/java/com/service/backend/FitnessTrackingApplication.java
+++ src/main/java/com/service/backend/FitnessTrackingApplication.java
@@ -3,6 +3,9 @@ package com.service.backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author Severiano Atencio
+ */
 @SpringBootApplication
 public class FitnessTrackingApplication {
 
