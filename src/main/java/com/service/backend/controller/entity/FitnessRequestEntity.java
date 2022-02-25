dab2c412c76825f725fae23635f8c9c9df7a7476
Severiano Atencio <satencio@consultec-ti.com>
2022-02-24 21:23:23 -0500

    add: common request entity

--- src/main/java/com/service/backend/controller/entity/FitnessRequestEntity.java
+++ src/main/java/com/service/backend/controller/entity/FitnessRequestEntity.java
@@ -1,4 +1,33 @@
 package com.service.backend.controller.entity;
 
-public class FitnessRequestEntity {
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+@JsonPropertyOrder({"body"})
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class FitnessRequestEntity<T> {
+
+    protected transient T body;
+
+    public FitnessRequestEntity(T body) {
+        this.body = body;
+    }
+
+    public FitnessRequestEntity() {
+    }
+
+    public T getBody() {
+        return body;
+    }
+
+    public void setBody(T body) {
+        this.body = body;
+    }
+
+    @Override
+    public String toString() {
+        return "FitnessRequestEntity{" +
+                "body=" + body +
+                '}';
+    }
 }
