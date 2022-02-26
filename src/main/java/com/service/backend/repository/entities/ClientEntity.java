623078124e7f7c1d816d5ef588fa9531dbbbd3eb
Severiano Atencio <satencio@consultec-ti.com>
2022-02-26 12:15:21 -0500

    add: repository y entity

---
+++ src/main/java/com/service/backend/repository/entities/ClientEntity.java
@@ -0,0 +1,42 @@
+package com.service.backend.repository.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.io.Serial;
+import java.io.Serializable;
+
+/**
+ * @author Severiano Atencio
+ */
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity(name = "client")
+@Table(name = "client")
+public class ClientEntity implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = -6959746196273371712L;
+
+    @Id
+    @Column(name = "id")
+    private String id;
+
+    @Column(name = "username")
+    private String username;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "age")
+    private Integer age;
+
+}
