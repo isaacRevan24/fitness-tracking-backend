82395922831d5a887e494284899936c0803441f3
Severiano Atencio <satencio@consultec-ti.com>
2022-02-26 14:29:44 -0500

    add: base signup controller

--- src/main/java/com/service/backend/controller/UserController.java
+++ src/main/java/com/service/backend/controller/UserController.java
@@ -1,13 +1,19 @@
 package com.service.backend.controller;
 
 import com.service.backend.controller.entity.BaseResponseEntity;
+import com.service.backend.controller.entity.FitnessRequestEntity;
 import com.service.backend.enums.StatusEnum;
 import com.service.backend.mapper.FitnessMapper;
+import com.service.backend.model.SignUpReqDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
 
 /**
  * @author Severiano Atencio
@@ -26,6 +32,15 @@ public class UserController {
         var response = new BaseResponseEntity(status);
 
         return ResponseEntity.status(status.getHttpStatus()).body(response);
+    }
 
+    @PostMapping(path = "/sign-up")
+    public ResponseEntity<BaseResponseEntity> signUp(@Valid @RequestBody FitnessRequestEntity<SignUpReqDTO> body) {
+
+        var response = new BaseResponseEntity();
+
+        response.setStatus(mapper.toStatusDTO(StatusEnum.SUCCESS));
+
+        return ResponseEntity.status(response.getStatus().getHttpStatus()).body(response);
     }
 }
