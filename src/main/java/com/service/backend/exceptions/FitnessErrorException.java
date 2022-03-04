6ee6f233ffb4f7ef5374391a8b6eb415d40587e0
Severiano Atencio <satencio@consultec-ti.com>
2022-03-03 19:16:01 -0500

    change: generic status error

--- src/main/java/com/service/backend/exceptions/FitnessErrorException.java
+++ src/main/java/com/service/backend/exceptions/FitnessErrorException.java
@@ -1,6 +1,7 @@
 package com.service.backend.exceptions;
 
 import lombok.Getter;
+import org.springframework.http.HttpStatus;
 
 import java.io.Serial;
 
@@ -15,16 +16,26 @@ public class FitnessErrorException extends RuntimeException {
 
     private final String errorCode;
     private final String errorMessage;
+    private final HttpStatus httpError;
 
-    public FitnessErrorException(String errorCode, String errorMessage, Throwable cause) {
-        super(errorMessage, cause);
-
+    public FitnessErrorException(Throwable cause, String errorCode, String errorMessage, HttpStatus httpError) {
+        super(cause);
         this.errorCode = errorCode;
         this.errorMessage = errorMessage;
+        this.httpError = httpError;
+    }
+
+    public FitnessErrorException(String message, Throwable cause, String errorCode, String errorMessage, HttpStatus httpError) {
+        super(message, cause);
+        this.errorCode = errorCode;
+        this.errorMessage = errorMessage;
+        this.httpError = httpError;
     }
 
     public FitnessErrorException() {
         this.errorCode = null;
         this.errorMessage = null;
+        this.httpError = null;
     }
+
 }
