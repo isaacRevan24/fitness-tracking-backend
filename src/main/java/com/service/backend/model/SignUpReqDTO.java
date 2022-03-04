59b59696b1355274dcbca453f1138cb354604f05
Severiano Atencio <satencio@consultec-ti.com>
2022-03-03 19:05:33 -0500

    change: deleted unnecesary parameters

--- src/main/java/com/service/backend/model/SignUpReqDTO.java
+++ src/main/java/com/service/backend/model/SignUpReqDTO.java
@@ -30,18 +30,6 @@ public class SignUpReqDTO implements Serializable {
     @NotBlank
     private String password;
 
-    @Size(min = 2, max = 2)
-    private String weightUnit;
-
-    @NotNull
-    private Double weight;
-
-    @Size(min = 2, max = 2)
-    private String heightUnit;
-
-    @NotNull
-    private Double height;
-
     private Integer age;
 
 }
