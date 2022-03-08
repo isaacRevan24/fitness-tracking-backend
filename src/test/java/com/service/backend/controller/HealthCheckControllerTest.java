e8e1dedade20ffea513cb5ad3643ccedb86aaf4e
Severiano Atencio <satencio@consultec-ti.com>
2022-03-08 18:35:29 -0500

    remove: unused import removed

--- src/test/java/com/service/backend/controller/HealthCheckControllerTest.java
+++ src/test/java/com/service/backend/controller/HealthCheckControllerTest.java
@@ -1,18 +1,12 @@
 package com.service.backend.controller;
 
 import com.service.backend.FitnessTrackingApplication;
-import com.service.backend.command.FitnessCommand;
 import com.service.backend.enums.StatusEnum;
-import com.service.backend.mapper.FitnessMapper;
-import com.service.backend.model.SignUpReqDTO;
-import com.service.backend.model.StatusDTO;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -29,13 +23,6 @@ public class HealthCheckControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
-
-    @MockBean
-    @Qualifier("SignUpCommand")
-    private FitnessCommand<SignUpReqDTO, StatusDTO> signUpCommand;
-
-    @Autowired
-    private FitnessMapper mapper;
 
     @Test
     void itShouldHealthCheckSuccessfully() throws Exception {
