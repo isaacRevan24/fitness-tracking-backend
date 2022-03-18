eac618f3ed5e33ec1f98146266699ba871d67412
Severiano Atencio <satencio@consultec-ti.com>
2022-03-18 17:28:07 -0500

    add: encrypt method

--- src/main/java/com/service/backend/util/crypto/impl/EncryptionUtilImpl.java
+++ src/main/java/com/service/backend/util/crypto/impl/EncryptionUtilImpl.java
@@ -1,9 +1,13 @@
 package com.service.backend.util.crypto.impl;
 
+import com.service.backend.enums.GenericLogEnum;
 import com.service.backend.exceptions.FitnessErrorException;
 import com.service.backend.util.crypto.EncryptionUtil;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.crypto.bcrypt.BCrypt;
 import org.springframework.stereotype.Component;
+
+import java.util.Base64;
 
 /**
  * @author Severiano Atencio
@@ -12,9 +16,22 @@ import org.springframework.stereotype.Component;
 @Component("EncryptionUtilImpl")
 public class EncryptionUtilImpl implements EncryptionUtil {
 
+    private static final int ITERATIONS = 12;
+
     @Override
     public String encryptPassword(String password) throws FitnessErrorException {
-        return null;
+
+        final var methodName = "encryptPassword";
+
+        log.debug(GenericLogEnum.START_MESSAGE.getMessage() + methodName);
+
+        var hash = BCrypt.hashpw(password, BCrypt.gensalt(ITERATIONS));
+
+        var encodedHash = Base64.getEncoder().encodeToString(hash.getBytes());
+
+        log.debug(GenericLogEnum.START_MESSAGE.getMessage() + methodName);
+
+        return encodedHash;
     }
 
     @Override
