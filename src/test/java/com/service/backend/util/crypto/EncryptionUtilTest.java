15e3bfee2163cc5278864ef9ff69b9ed0772e23d
Severiano Atencio <satencio@consultec-ti.com>
2022-03-18 17:34:36 -0500

    add: encryption test

---
+++ src/test/java/com/service/backend/util/crypto/EncryptionUtilTest.java
@@ -0,0 +1,28 @@
+package com.service.backend.util.crypto;
+
+import com.service.backend.FitnessTrackingApplication;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(classes = {FitnessTrackingApplication.class}, properties = "spring.main.allow-bean-definition-overriding=true")
+class EncryptionUtilTest {
+
+    @Autowired
+    private EncryptionUtil underTest;
+
+    @Test
+    void itShouldGenerateHashedPassword() {
+        // Given
+        final var password = "Password123*";
+
+        // When
+        var response = underTest.encryptPassword(password);
+
+        // Then
+        assertThat(response).isBase64();
+
+    }
+}
