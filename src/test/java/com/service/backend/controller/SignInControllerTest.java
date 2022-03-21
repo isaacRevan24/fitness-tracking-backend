047a16a57d88b889685bce2233bee10d641d3bbc
Severiano Atencio <satencio@consultec-ti.com>
2022-03-20 22:16:01 -0500

    add: controller and test for controller signin

--- src/test/java/com/service/backend/controller/SignInControllerTest.java
+++ src/test/java/com/service/backend/controller/SignInControllerTest.java
@@ -1,2 +1,47 @@
-package com.service.backend.controller;public class SignInControllerTest {
+package com.service.backend.controller;
+
+import com.service.backend.FitnessTrackingApplication;
+import com.service.backend.enums.StatusEnum;
+import com.service.backend.mapper.FitnessMapper;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.Matchers.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest(classes = {FitnessTrackingApplication.class}, properties = "spring.main.allow-bean-definition-overriding=true")
+@AutoConfigureMockMvc
+class SignInControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private FitnessMapper mapper;
+
+    @Test
+    void itShouldMakeRequestSuccessfully() throws Exception {
+        // Given
+        final var bodyRequest = "{ \"body\": { \"username\": \"aizack\", \"password\": \"password123\" } }";
+
+        // When
+        // Then
+        mockMvc.perform(post("/user/sign-in")
+                        .header("Content-Type", "application/json")
+                        .content(bodyRequest)
+                )
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.status.code", Matchers.is(StatusEnum.SUCCESS.getCode())))
+                .andExpect(jsonPath("$.status.message", is(StatusEnum.SUCCESS.getMessage())));
+
+    }
 }
