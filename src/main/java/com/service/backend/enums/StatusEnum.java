61ec0f6b202858cf18aace50f10e5f1b932f59d7
isaacRevan24 <isaac.atencio2498@gmail.com>
2022-05-12 19:12:41 -0500

    add: enum for signin responses

--- src/main/java/com/service/backend/enums/StatusEnum.java
+++ src/main/java/com/service/backend/enums/StatusEnum.java
@@ -14,7 +14,9 @@ public enum StatusEnum {
     INTERNAL_ERROR("FIT-02", "Internal error.", HttpStatus.INTERNAL_SERVER_ERROR),
     ENCRYPTION_EXCEPTION("FIT-03", "Encryption error.", HttpStatus.INTERNAL_SERVER_ERROR),
     INVALID_PASSWORD_FORMAT("FIT-04", "Invalid password format.", HttpStatus.INTERNAL_SERVER_ERROR),
-    ERROR_GETTING_PASSWORD("FIT-05", "Error getting the password.", HttpStatus.INTERNAL_SERVER_ERROR);
+    ERROR_GETTING_PASSWORD("FIT-05", "Error getting the password.", HttpStatus.INTERNAL_SERVER_ERROR),
+    ERROR_VALIDATION_PASSWORD("FIT-06", "Error validating password.", HttpStatus.INTERNAL_SERVER_ERROR),
+    SIGN_IN_ERROR("FIT-07", "Cant sign in.", HttpStatus.BAD_REQUEST);
 
     private final String code;
     private final String message;
