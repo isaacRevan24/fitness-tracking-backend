6443215f20b716d296730cacc488590564b21e41
isaacRevan24 <isaac.atencio2498@gmail.com>
2022-05-12 19:54:55 -0500

    add: sign in to controller + test

--- src/test/java/com/service/backend/controller/SignInControllerTest.java
+++ src/test/java/com/service/backend/controller/SignInControllerTest.java
@@ -1,17 +1,25 @@
 package com.service.backend.controller;
 
 import com.service.backend.FitnessTrackingApplication;
+import com.service.backend.command.FitnessCommand;
+import com.service.backend.controller.entity.FitnessResponseEntity;
 import com.service.backend.enums.StatusEnum;
 import com.service.backend.mapper.FitnessMapper;
+import com.service.backend.model.SignInReqDTO;
+import com.service.backend.model.SignInResDTO;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.hamcrest.Matchers.is;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doReturn;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -27,10 +35,20 @@ class SignInControllerTest {
     @Autowired
     private FitnessMapper mapper;
 
+    @MockBean
+    @Qualifier("SignInCommand")
+    private FitnessCommand<SignInReqDTO, SignInResDTO> signInCommand;
+
     @Test
     void itShouldMakeRequestSuccessfully() throws Exception {
         // Given
         final var bodyRequest = "{ \"body\": { \"username\": \"aizack\", \"password\": \"password123\" } }";
+
+        // Mock
+        final var mockResponse = new FitnessResponseEntity<SignInResDTO>();
+        mockResponse.setStatus(mapper.toStatusDTO(StatusEnum.SUCCESS));
+        mockResponse.setBody(new SignInResDTO("c08cb8f4-ca26-4635-8f13-0270ef8ea371"));
+        doReturn(mockResponse).when(signInCommand).execute(any());
 
         // When
         // Then
