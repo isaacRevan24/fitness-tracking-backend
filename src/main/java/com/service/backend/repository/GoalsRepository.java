86c1f3914e48e25738a945aa568f1ff5c230be1f
Severiano Atencio <satencio@consultec-ti.com>
2022-06-06 13:32:56 -0500

    remove: import

--- src/main/java/com/service/backend/repository/GoalsRepository.java
+++ src/main/java/com/service/backend/repository/GoalsRepository.java
@@ -1,6 +1,5 @@
 package com.service.backend.repository;
 
-import com.service.backend.repository.entities.ClientEntity;
 import com.service.backend.repository.entities.GoalsEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
