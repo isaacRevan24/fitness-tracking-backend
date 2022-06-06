679da7626c909ca18e7974038ff2d0c01689731c
Severiano Atencio <satencio@consultec-ti.com>
2022-06-06 12:32:40 -0500

    add: goals entity db

---
+++ src/main/java/com/service/backend/repository/entities/GoalsEntity.java
@@ -0,0 +1,40 @@
+package com.service.backend.repository.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.io.Serial;
+import java.io.Serializable;
+import java.util.UUID;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity(name = "goals")
+@Table(name = "goals")
+public class GoalsEntity implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 693609396463190403L;
+
+    @Id
+    @Column(name = "id")
+    private UUID id;
+
+    @Column(name = "weight")
+    private Double weight;
+
+    @Column(name = "steps")
+    private Integer steps;
+
+}
