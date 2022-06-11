bcb1a8b701e4f3579ddca91a95aac2b40169e333
Severiano Atencio <satencio@consultec-ti.com>
2022-06-11 13:35:16 -0500

    add: interface for get goals

--- src/main/java/com/service/backend/logic/GetGoalsLogic.java
+++ src/main/java/com/service/backend/logic/GetGoalsLogic.java
@@ -1,2 +1,13 @@
-package com.service.backend.logic;public interface GetGoalsLogic {
+package com.service.backend.logic;
+
+import com.service.backend.exceptions.FitnessErrorException;
+import com.service.backend.model.GoalsResDTO;
+
+/**
+ * @author Severiano Atencio
+ */
+public interface GetGoalsLogic {
+
+    GoalsResDTO getGoals(String clientId) throws FitnessErrorException;
+
 }
