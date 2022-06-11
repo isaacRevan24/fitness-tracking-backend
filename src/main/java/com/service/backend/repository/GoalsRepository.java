b001306055de79b16830f3b8e161dfc61a90825f
Severiano Atencio <satencio@consultec-ti.com>
2022-06-11 14:48:56 -0500

    change: change string to uuid

--- src/main/java/com/service/backend/repository/GoalsRepository.java
+++ src/main/java/com/service/backend/repository/GoalsRepository.java
@@ -5,6 +5,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.UUID;
+
 /**
  * @author Severiano Atencio
  */
@@ -12,6 +14,6 @@ import org.springframework.stereotype.Repository;
 public interface GoalsRepository extends JpaRepository<GoalsEntity, Long> {
 
     @Query(value = "SELECT * FROM goals WHERE id=?1", nativeQuery = true)
-    GoalsEntity fetchUserGoals(String clientId);
+    GoalsEntity fetchUserGoals(UUID clientId);
 
 }
