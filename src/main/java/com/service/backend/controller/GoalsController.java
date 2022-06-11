5b946a7e266a396a0a61c4c2c9b70837af8e1f84
Severiano Atencio <satencio@consultec-ti.com>
2022-06-11 11:23:24 -0500

    add: base controller for get goals

--- src/main/java/com/service/backend/controller/GoalsController.java
+++ src/main/java/com/service/backend/controller/GoalsController.java
@@ -4,6 +4,7 @@ import com.service.backend.command.FitnessCommand;
 import com.service.backend.controller.entity.BaseResponseEntity;
 import com.service.backend.controller.entity.FitnessRequestEntity;
 import com.service.backend.controller.entity.FitnessResponseEntity;
+import com.service.backend.enums.StatusEnum;
 import com.service.backend.mapper.FitnessMapper;
 import com.service.backend.model.AddGoalsReqDTO;
 import com.service.backend.model.GoalsResDTO;
@@ -11,6 +12,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -40,9 +43,13 @@ public class GoalsController {
         return ResponseEntity.status(response.getStatus().getHttpStatus()).body(response);
     }
 
-    public ResponseEntity<BaseResponseEntity> getGoals() {
+    @GetMapping(path = "/{id}")
+    public ResponseEntity<FitnessResponseEntity<GoalsResDTO>> getGoals(@PathVariable String id) {
 
-        return ResponseEntity.status(HttpStatus.OK).body(null);
+        var response = new FitnessResponseEntity<GoalsResDTO>();
+        response.setBody(new GoalsResDTO(95.00, 12000));
+        response.setStatus(mapper.toStatusDTO(StatusEnum.SUCCESS));
 
+        return ResponseEntity.status(HttpStatus.OK).body(response);
     }
 }
