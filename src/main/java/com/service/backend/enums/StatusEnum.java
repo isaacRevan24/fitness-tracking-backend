1685e9f7ce6ab1ffa361ed775cd0ac1be319e05b
Severiano Atencio <satencio@consultec-ti.com>
2022-06-18 18:48:24 -0500

    add: enum for updating steps goals

--- src/main/java/com/service/backend/enums/StatusEnum.java
+++ src/main/java/com/service/backend/enums/StatusEnum.java
@@ -20,7 +20,8 @@ public enum StatusEnum {
     NOT_MATCHING_RECORD("FIT-08", "Can't find user goals record.", HttpStatus.BAD_REQUEST),
     INVALID_WEIGHT_GOAL("FIT-09", "Invalid weight goal.", HttpStatus.BAD_REQUEST),
     INVALID_STEP_GOAL("FIT-10", "Invalid step goal.", HttpStatus.BAD_REQUEST),
-    ERROR_UPDATING_WEIGHT("FIT-10", "Error trying to update weight.", HttpStatus.BAD_REQUEST);
+    ERROR_UPDATING_WEIGHT("FIT-11", "Error trying to update weight goal.", HttpStatus.BAD_REQUEST),
+    ERROR_UPDATING_STEPS("FIT-12", "Error trying to update steps goal.", HttpStatus.BAD_REQUEST);
 
     private final String code;
     private final String message;
