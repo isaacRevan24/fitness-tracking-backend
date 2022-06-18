6b9d632e9c06a0de37e805baac2860ac6a2f8603
Severiano Atencio <satencio@consultec-ti.com>
2022-06-18 18:29:50 -0500

    add: dto for update steps goal

---
+++ src/main/java/com/service/backend/model/UpdateStepsGoalReq.java
@@ -0,0 +1,52 @@
+package com.service.backend.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.Builder;
+import lombok.ToString;
+
+import javax.validation.constraints.NotNull;
+import java.io.Serial;
+import java.io.Serializable;
+import java.util.UUID;
+
+/**
+ * @author Severiano Atencio
+ */
+@Builder
+@ToString
+public class UpdateStepsGoalReq implements Serializable {
+
+    @Serial
+    @JsonIgnore
+    private static final long serialVersionUID = 273107109979090281L;
+
+    @NotNull
+    private UUID clientId;
+
+    @NotNull
+    private Integer stepsGoal;
+
+    public UpdateStepsGoalReq(UUID clientId, Integer stepsGoal) {
+        this.clientId = clientId;
+        this.stepsGoal = stepsGoal;
+    }
+
+    public UpdateStepsGoalReq() {
+    }
+
+    public UUID getClientId() {
+        return clientId;
+    }
+
+    public void setClientId(UUID clientId) {
+        this.clientId = clientId;
+    }
+
+    public Integer getStepsGoal() {
+        return stepsGoal;
+    }
+
+    public void setStepsGoal(Integer stepsGoal) {
+        this.stepsGoal = stepsGoal;
+    }
+}
