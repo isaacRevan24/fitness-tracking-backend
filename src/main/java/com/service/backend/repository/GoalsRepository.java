ecfc1f53acb1754e8b5c82e446435bcfdc8b8866
Severiano Atencio <satencio@consultec-ti.com>
2022-06-18 16:24:31 -0500

    add: update query

--- src/main/java/com/service/backend/repository/GoalsRepository.java
+++ src/main/java/com/service/backend/repository/GoalsRepository.java
@@ -2,6 +2,7 @@ package com.service.backend.repository;
 
 import com.service.backend.repository.entities.GoalsEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
@@ -15,5 +16,9 @@ public interface GoalsRepository extends JpaRepository<GoalsEntity, Long> {
 
     @Query(value = "SELECT * FROM goals WHERE id=?1", nativeQuery = true)
     GoalsEntity fetchUserGoals(UUID clientId);
+
+    @Modifying
+    @Query(value = "UPDATE goals SET weight=?2 WHERE id=?1", nativeQuery = true)
+    int updateWeighGoal(UUID clientId,  Double weight);
 
 }
