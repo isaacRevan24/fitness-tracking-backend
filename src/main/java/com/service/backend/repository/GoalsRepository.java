cd19f201435659636133a2b858ce8056fadb4abb
Severiano Atencio <satencio@consultec-ti.com>
2022-06-18 18:03:31 -0500

    add: transactional

--- src/main/java/com/service/backend/repository/GoalsRepository.java
+++ src/main/java/com/service/backend/repository/GoalsRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.UUID;
 
@@ -17,6 +18,7 @@ public interface GoalsRepository extends JpaRepository<GoalsEntity, Long> {
     @Query(value = "SELECT * FROM goals WHERE id=?1", nativeQuery = true)
     GoalsEntity fetchUserGoals(UUID clientId);
 
+    @Transactional
     @Modifying
     @Query(value = "UPDATE goals SET weight=?2 WHERE id=?1", nativeQuery = true)
     int updateWeighGoal(UUID clientId,  Double weight);
