bf0e87c5d999e134ee34fcc897741d36ae35a74c
Severiano Atencio <satencio@consultec-ti.com>
2022-06-18 15:57:07 -0500

    change: refactor for all user in one interface

--- src/main/java/com/service/backend/logic/impl/UserLogicImpl.java
+++ src/main/java/com/service/backend/logic/impl/UserLogicImpl.java
@@ -2,13 +2,17 @@ package com.service.backend.logic.impl;
 
 import com.service.backend.enums.GenericLogEnum;
 import com.service.backend.exceptions.FitnessErrorException;
-import com.service.backend.logic.SignInLogic;
+import com.service.backend.logic.UserLogic;
+import com.service.backend.mapper.FitnessMapper;
+import com.service.backend.model.SignUpReqDTO;
 import com.service.backend.repository.ClientRepository;
+import com.service.backend.repository.ClientValuesRepository;
 import com.service.backend.repository.entities.ClientEntity;
 import com.service.backend.util.crypto.EncryptionUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 import static com.service.backend.enums.StatusEnum.DATABASE_ERROR;
 import static com.service.backend.enums.StatusEnum.ENCRYPTION_EXCEPTION;
@@ -17,14 +21,69 @@ import static com.service.backend.enums.StatusEnum.ENCRYPTION_EXCEPTION;
  * @author Severiano Atencio
  */
 @Slf4j
-@Component("SignInLogicImpl")
-public class SignInLogicImpl implements SignInLogic {
+@Component("SignUpLogicImpl")
+public class UserLogicImpl implements UserLogic {
 
     @Autowired
     private ClientRepository repository;
 
     @Autowired
+    private ClientValuesRepository clientValuesRepository;
+
+    @Autowired
     private EncryptionUtil encryptionUtil;
+
+    @Autowired
+    private FitnessMapper mapper;
+
+    @Override
+    public String encryptPassword(String password) throws FitnessErrorException {
+
+        final var methodName = "encryptPassword";
+
+        log.debug(GenericLogEnum.START_MESSAGE.getMessage() + methodName);
+
+        String encryptedPassword;
+
+        try {
+
+            encryptedPassword = encryptionUtil.encryptPassword(password);
+
+        } catch (Exception exception) {
+
+            log.error(exception.getMessage());
+
+            throw new FitnessErrorException(
+                    exception.getMessage(),
+                    exception,
+                    ENCRYPTION_EXCEPTION.getCode(),
+                    ENCRYPTION_EXCEPTION.getMessage(),
+                    ENCRYPTION_EXCEPTION.getStatus()
+            );
+
+        }
+
+        log.debug(GenericLogEnum.FINISH_MESSAGE.getMessage() + methodName);
+
+        return encryptedPassword;
+    }
+
+    @Override
+    @Transactional(rollbackFor = {Exception.class, FitnessErrorException.class})
+    public void signUp(SignUpReqDTO request) throws FitnessErrorException {
+
+        final var methodName = "encryptPassword";
+
+        log.debug(GenericLogEnum.START_MESSAGE.getMessage() + methodName);
+
+        final var clientEntity = mapper.toClientEntity(request);
+
+        final var client = repository.save(clientEntity);
+
+        clientValuesRepository.save(mapper.toClientValuesEntity(request, client.getId()));
+
+        log.debug(GenericLogEnum.FINISH_MESSAGE.getMessage() + methodName);
+    }
 
     @Override
     public ClientEntity getUser(String username) throws FitnessErrorException {
--- src/test/java/com/service/backend/logic/SignUpLogicTest.java
+++ src/test/java/com/service/backend/logic/SignUpLogicTest.java
@@ -29,7 +29,7 @@ class SignUpLogicTest {
     private ClientValuesRepository clientValuesRepository;
 
     @Autowired
-    private SignUpLogic underTest;
+    private UserLogic underTest;
 
     @MockBean
     private EncryptionUtil encryptionUtil;
